839767c190b052e69c7fcb00796c417b0cfed219
Dante Castro Garro <dante.tcg@gmail.com>
2022-01-13 14:48:51 +0100

    Adding wind data process for assimilatio AMM.

--- setup.sh
+++ setup.sh
@@ -4,15 +4,6 @@
 # Setup directories for the project.
 # This script requires 'cdo' and 'ncks' (by doing `conda activate py39`)
 #
-# data
-# |
-# +-- sst
-# +-- prec
-# +-- wind
-# +-- index
-#
-# NOTE: For the processing of wind data you need to activate
-#
 # Author: Dante T. Castro Garro
 # Date: 2021-12-23
 # -----------------------------------------------------------------------------
@@ -32,6 +23,9 @@ cp $ruta"/masks/india_mask_MR30.nc" "../data/mask/"
 cp $ruta"/GPCP/GPCP__V2_3__PRECIP__2.5x2.5_sel.nc" "../data/prec/"
 cp $ruta"/SST/noaa-oisst_sst_1982-2021-mon.nc" "../data/sst/"
 cp $ruta"/wind/era5_uwind_1981-2016_lev-200-850.nc" "../data/wind/"
+cp $ruta"/SST/noaa-ersstv5_sst_1854-2021-mon_2p5.nc" "../data/sst/"
+cp $ruta"/wind/ncep-ncar_uwind_1948-2021-mon_10m.nc" "../data/wind/"
+cp $ruta"/wind/ncep-ncar_vwind_1948-2021-mon_10m.nc" "../data/wind/"
 
 # Precipitation time series
 ./prec_ts.sh obs month
@@ -53,3 +47,8 @@ julia --project=../ data_index.jl oni
 julia --project=../ data_index.jl dmi
 julia --project=../ data_index.jl wio
 julia --project=../ data_index.jl wyi
+
+# Calculate AMM index
+python3 amm_index_obs.py
+python3 amm_index_asm.py
+python3 amm_index_mem.py
