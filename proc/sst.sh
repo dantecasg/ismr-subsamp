885f70c55a58189ffc4c9a19776ba5570cc1a6fb
Dante Castro Garro <dante.tcg@gmail.com>
2022-01-16 10:26:07 +0100

    Updating SST calculation

--- proc/sst.sh
+++ proc/sst.sh
@@ -21,19 +21,19 @@ ruta="../data/sst"
 case $1 in
     obs)
         data="/pool/data/ICDC/ocean/hadisst1/DATA/HadISST_sst.nc"
-        out=$ruta"/hadisst-obs_sst_1981-2016"
+        out=${ruta}"/hadisst-obs_sst_1981-2016"
         # out="${ruta}/SST/hadisst_sst_1981-2016"
         ;;
 
     asm)
         data="/work/uo1075/u241265/post_defense/monsson_subsampling/MR30_assimilation/dante/asOEffESIP_r1i1p1-MR_mpiom_data_2d_mm_sst_1980_2017.nc"
-        out=$ruta/"mpi-om-asm_sst_1981-2016"
+        out=${ruta}/"mpi-om-asm_sst_1981-2016"
         # out="${ruta}/SST/mpi-om-asm_sst_1981-2016"
         ;;
 
     ens)
         data="/work/uo1075/u241265/post_defense/monsson_subsampling/MR30_hindcast/dante/hcassimbt_r1-30_i1p1-MR_mpiom_data_2d_sst_1980_2016.nc"
-        out=$ruta/"mpi-om-ens_sst_1981-2016"
+        out=${ruta}/"mpi-om-ens_sst_1981-2016"
         # out="${ruta}/SST/mpi-om-ens_sst_1981-2016"
         ;;
 
@@ -48,9 +48,11 @@ case $2 in
     month)
         cdo --reduce_dim -remapbil,r180x90 -monmean -selyear,1981/2016 -selname,sst ${data} ${out}-mon.nc
         ;;
-    # jja)
-    #     cdo --reduce_dim -remapbil,r180x90 -yearmean -monmean -selseason,JJA -selyear,1981/2016 -selname,sst ${data} ${out}-JJA.nc
-    #     ;;
+
+    jja)
+        cdo --reduce_dim -remapbil,r180x90 -yearmean -monmean -selseason,JJA -selyear,1981/2016 -selname,sst ${data} ${out}-JJA-cli.nc
+        ;;
+
     *) # Default
         echo "No method was selected"
         exit
